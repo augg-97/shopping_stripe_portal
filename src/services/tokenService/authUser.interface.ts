59535a2f98a0306003d2cf1e9f65159172cd17c3
Double.H <lhhoang98197@gmail.com>
2023-12-25 16:27:59 +0700

    fix token service

--- src/services/tokenService/authUser.interface.ts
+++ src/services/tokenService/authUser.interface.ts
@@ -1,4 +1,14 @@
+import { REDIS_KEY } from "../redisService/redisKey";
+
 export interface IAuthUser {
   id: string;
   email: string;
 }
+
+export const MODULE_TOKEN = "MODULE_TOKEN";
+
+export type TokenInfo = {
+  redisKey: REDIS_KEY;
+  key: string;
+  expiration: number;
+};
